39baff46ad2e19474a0f9ffbafeb19f423c156f6
Daniel Firu <daniel@quadric.io>
2023-12-08 14:27:51 -0800

    put bot on explicitly for now

--- miryoku/miryoku_shift_functions.h
+++ miryoku/miryoku_shift_functions.h
@@ -14,7 +14,7 @@
   #define U_RGB_TOG &rgb_ug RGB_TOG
   #define U_RGB_EFF &rgb_ug RGB_EFF
   #define U_RGB_HUI &rgb_ug RGB_HUI
-  #define U_RGB_SAI &rgb_ug RGB_SAI
+  #define U_RGB_SAI &rgb_ug RGB_BRI
   #define U_RGB_BRI &rgb_ug RGB_BRD
   #define U_EP_TOG &ext_power EP_TOG
 #endif
